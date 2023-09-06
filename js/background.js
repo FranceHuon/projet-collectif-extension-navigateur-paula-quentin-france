26b2dcae09c8d73e3f059669732389873ac51cbb
quentinkerzerho <quentin.kerzerho@gmail.com>
2023-09-06 16:53:22 +0200

    Affichage des personnes nés dans le mois en cours

--- js/background.js
+++ js/background.js
@@ -2,6 +2,23 @@ const apiKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxZnNqdWZzdXV0bW52endpbnVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5MDE5NTgsImV4cCI6MjAwOTQ3Nzk1OH0.-rasUQus-Zlolv_Q1URFeR8XzMi-isSk5wC7xxCgbNc";
 const supaBaseURL = "https://aqfsjufsuutmnvzwinus.supabase.co/rest/v1/birthday";
 
+let monthOfTheYear = [
+  "Janvier",
+  "Février",
+  "Mars",
+  "Avril",
+  "Mai",
+  "Juin",
+  "Juillet",
+  "Août",
+  "Septembre",
+  "Octobre",
+  "Novembre",
+  "Décembre",
+];
+let monthActual = new Date().getMonth() + 1;
+console.log(monthActual);
+
 async function getBirthdays() {
   const response = await fetch(supaBaseURL, {
     headers: {
@@ -10,6 +27,30 @@ async function getBirthdays() {
     },
   });
   const data = await response.json();
-  console.log (data)
+
+  function getBirthdayMonth(x) {
+    let dateOfBirthday = new Date(data[x]["birthday"]);
+    let monthOfBirthday = dateOfBirthday.getMonth() + 1;
+    return monthOfBirthday;
+  }
+  let birthdayDiv;
+  for (let i = 0; i < data.length; i++) {
+    if (getBirthdayMonth(i) === monthActual) {
+      birthdayDiv = document.createElement("div");
+      document.querySelector("#birthday").appendChild(birthdayDiv);
+      birthdayDiv.innerHTML =
+        "C'est l'anniversaire de " + data[i]["name"] + data[i]["birthday"];
+    }
+  }
 }
-getBirthdays()
+getBirthdays();
+
+// let tabBirthday = [];
+// for (let i = 0; i < data.length; i++) {
+//   if (getBirthdayMonth(i) === monthActual) {
+//     let test = data[i]["name"] + data[i]["birthday"];
+//     tabBirthday.push(test);
+//     let birthday = document.querySelector("#birthday");
+//     birthday.innerHTML = "C'est l'anniversaire de " + "<br>" + tabBirthday;
+//   }
+// }
